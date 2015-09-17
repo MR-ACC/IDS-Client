e900f730fe51d25c2ea21596d924ea490edffb69
Rong Peng <rongpmcu@gmail.com>
2015-09-17 22:23:52 +0800

    1> adjust client gui 2> add reconnect handler 3> remove some build warning
    Signed-off-by: Rong Peng <rongpmcu@gmail.com>

--- ids_client/idsclient.cpp
+++ ids_client/idsclient.cpp
@@ -8,13 +8,8 @@
 
 void ids_io_fin_cb(gpointer priv)
 {
-    ((idsclient*)priv)->mIdsEndpoint = ids_create_remote_endpoint(((idsclient*)priv)->mIp, SERVER_PORT, ids_io_fin_cb, priv, NULL);
-    qDebug() << "sdflksjdfsssssssssssssss";
-    if (NULL == ((idsclient*)priv)->mIdsEndpoint)
-    {
-        QMessageBox::information(NULL, "tips", QString().sprintf("connect to server(%s) failed.", ((idsclient*)priv)->mIp), QMessageBox::Yes, NULL);
-    }
-    //QMessageBox::information(NULL, "tips", QString().sprintf("reconnect to server(%s) successed.", ((idsclient*)priv)->mIp), QMessageBox::Yes, NULL);
+    ((idsclient*)priv)->mIdsEndpoint = NULL;  //lock mIdsEndpoint before use it???
+    emit ((idsclient*)priv)->connect_network(1);
 }
 
 idsclient::idsclient(QWidget *parent) :
@@ -25,14 +20,41 @@ idsclient::idsclient(QWidget *parent) :
 
     if (TRUE != ids_core_init())
         throw QString("init core library failed");
+
     if (TRUE != ids_modules_init())
         throw QString("init modules library failed");
+
     mIdsEndpoint = NULL;
+
+    connect(this, SIGNAL(connect_network(int)), this, SLOT(connect_server(int)));
 }
 
 idsclient::~idsclient()
 {
-    delete ui;
+    //id_modules_uninit();
+    //ids_core_uninit();
+    //delete ui;
+}
+
+void idsclient::connect_server(int prompt_first)
+{
+    if (prompt_first)
+    {
+        QMessageBox::StandardButton rb = QMessageBox::question(NULL, "Tips"
+                           , "Currently the network is disconnected, connect it now?"
+                           , QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+        if(rb != QMessageBox::Yes)
+            return ;
+    }
+
+    if (mIdsEndpoint != NULL)
+        ids_destroy_remote_endpoint(mIdsEndpoint);
+
+    mIdsEndpoint = ids_create_remote_endpoint(mIp, SERVER_PORT, ids_io_fin_cb, this, NULL);
+    if (NULL == mIdsEndpoint)
+        QMessageBox::critical(NULL, "Error", QString().sprintf("connect to server(%s) failed.", mIp));
+    else
+        QMessageBox::information(NULL, "tips", QString().sprintf("connect to server(%s) succeed.", mIp));
 }
 
 void idsclient::on_pushButton_connect_clicked()
@@ -40,15 +62,7 @@ void idsclient::on_pushButton_connect_clicked()
     strcpy(mIp, ui->lineEdit_ip->text().toLatin1());
     qDebug() <<"server ip:" << mIp;
 
-    if (mIdsEndpoint != NULL)
-        ids_destroy_remote_endpoint(mIdsEndpoint);
-    mIdsEndpoint = ids_create_remote_endpoint(mIp, SERVER_PORT, ids_io_fin_cb, this, NULL);
-    if (NULL == mIdsEndpoint)
-    {
-        QMessageBox::information(NULL, "tips", QString().sprintf("connect to server(%s) failed.", mIp), QMessageBox::Yes, NULL);
-    }
-    else
-        QMessageBox::information(NULL, "tips", QString().sprintf("connect to server(%s) succeed.", mIp), QMessageBox::Yes, NULL);
+    connect_server(0);
 }
 
 void idsclient::on_pushButton_netcfg_clicked()
@@ -57,24 +71,24 @@ void idsclient::on_pushButton_netcfg_clicked()
 
     if (mIdsEndpoint == NULL)
     {
-        QMessageBox::information(NULL, "tips", "please connect first.", QMessageBox::Yes, NULL);
+        QMessageBox::information(NULL, "tips", "please connect first.");
         return ;
     }
 
     netCfg.update(mIdsEndpoint);
     netCfg.exec();
-    strcpy(mIp, netCfg.mNetInfo.ip);
 }
 
 void idsclient::on_pushButton_dispcfg_clicked()
 {
+    displayCfgDialog dispCfg;
+
     if (mIdsEndpoint == NULL)
     {
-        QMessageBox::information(NULL, "tips", "please connect first.", QMessageBox::Yes, NULL);
+        QMessageBox::information(NULL, "tips", "please connect first.");
         return ;
     }
-    displayCfgDialog dispCfg;
-    dispCfg.mIdsServerWin = this;
-    dispCfg.getInfo(mIdsEndpoint);
+
+    dispCfg.update(mIdsEndpoint);
     dispCfg.exec();
 }
--- ids_client/idsclient.h
+++ ids_client/idsclient.h
@@ -25,7 +25,10 @@ private slots:
     void on_pushButton_connect_clicked();
     void on_pushButton_netcfg_clicked();
     void on_pushButton_dispcfg_clicked();
+    void connect_server(int prompt_first);
 
+signals:
+    void connect_network(int);
 private:
     Ui::idsclient *ui;
 };
